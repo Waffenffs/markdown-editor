88c4ea88aef28568426e957122db52643e4cee6c
Waffen <waffen.sultanofc@gmail.com>
2023-03-31 15:52:26 +0800

    Tweaked some features

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -4,26 +4,37 @@ import { useState, useEffect } from 'react';
 import DOMPurify from 'dompurify'
 import '../App.css'
 
+// TO DO
+// create a database for each users
+// use firebase's firestore for the DB
+// use firebase's auth for user authentication
+// maybe use node.js firebase SDK?
+
 function Editor() {
+  // allows marked to render new lines
   marked.setOptions({ breaks: true })
 
+  // holds textarea content
   const [content, setContent] = useState<any>('');
 
+  // handles textarea changes
   const handleChange = (e: any) => {
     setContent(e.target.value)
   }
-
-  console.log(DOMPurify.sanitize(marked.parse(content.replace(/&gt;+/g, '>'))))
 
   return (
     <main>
       <div className="container">
         <article className='plain-text'>
+          <div className="header">MARKDOWN</div>
           <textarea onChange={handleChange} placeholder='Separate lines with two spaces'></textarea>
         </article>
 
         <article className="markdown">
-          <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(marked.parse(content.replace(/&gt;+/g, '>')))}} />
+          <div className="header">PREVIEW</div>
+          <div className="preview-container">
+            <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(marked.parse(content.replace(/&gt;+/g, '>')))}} />
+          </div>
         </article>
       </div>
     </main>
