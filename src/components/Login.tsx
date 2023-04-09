9e69cda4afc694b16f57d744f8d9b8bea2c1da62
Waffen <waffen.sultanofc@gmail.com>
2023-04-09 14:40:09 +0800

    Added registration & login page.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,20 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import '../App.css'
 
 function Login(props: any) {
+    const [loginEmail, setLoginEmail] = useState<string>('');
+    const [loginPasword, setLoginPassword] = useState<string>('');
+
     const handleClick = () => {
         props.changeStatus(true);
     }
     
     return (
         <article className="authentication">
-            <h1>Afin's MD</h1>
-            <p>Welcome back. 
-                <span onClick={handleClick} className='inquiry'>Don't own an account?</span>
+            <h1 className='auth-title'>Afin's MD</h1>
+            <p>Welcome back!
+                <span onClick={handleClick} className='inquiry'> Don't have an account already?</span>
             </p>
             <div className="part-container">
-                {/* add some stuff here e.g. password & email stuffs */}
+                <h5>EMAIL *</h5>
+                <input type="email" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value) } />
+            </div>
+            <div className="part-container">
+                <h5>PASSWORD *</h5>
+                <input type="password" value={loginPasword} onChange={(e) => setLoginPassword(e.target.value)} />
+            </div>
+            <div className="button-container">
+                <button>REGISTER</button>
             </div>
         </article>  
   )
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -2,19 +2,36 @@ import React from 'react'
 import { 
     createUserWithEmailAndPassword, 
      } from 'firebase/auth';
+import { useState } from 'react'
+import '../App.css'
 
 function Register(props: any) {
-    const handleClick = () => {
-        props.changeStatus(false)
-    }
+  // after registration, redirect user to home page
+
+  const [registerEmail, setRegisterEmail] = useState<string>('');
+  const [registerPassword, setRegisterPassword] = useState<string>('');
+
+  const handleClick = () => {
+      props.changeStatus(false)
+  }
 
   return (
     <article className="authentication">
-        <h1>Afin's MD</h1>
-        <p>Sign up now. 
-            <span onClick={handleClick} className='inquiry'>Already have an account?</span>
-        </p>
-        <div className="part-container"></div>
+      <h1 className='auth-title'>Afin's MD</h1>
+      <p>Sign up now.  
+          <span onClick={handleClick} className='inquiry'> Already have an account?</span>
+      </p>
+      <div className="part-container">
+        <h5>EMAIL *</h5>
+        <input type="email" value={registerEmail} onChange={(e) => setRegisterEmail(e.target.value) } />
+      </div>
+      <div className="part-container">
+        <h5>PASSWORD *</h5>
+        <input type="password" value={registerPassword} onChange={(e) => setRegisterPassword(e.target.value)} />
+      </div>
+      <div className="button-container">
+        <button>REGISTER</button>
+      </div>
     </article>
   )
 }
