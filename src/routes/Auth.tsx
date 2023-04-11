c4c51d91a16bba81b0da360c3f742e58df19de1f
Waffen <waffen.sultanofc@gmail.com>
2023-04-11 16:25:14 +0800

    Added routings and navigation

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -4,18 +4,22 @@ import '../App.css'
 import { useState } from 'react'
 import Register from '../components/Register';
 import Login from '../components/Login';
+import { AiOutlineRollback } from 'react-icons/ai'
+import { useNavigate } from 'react-router';
 
 function Auth() {
-    // TO DO:
-    // create a login/register page
-        // hook up to firebase authentication
-    // email
-    // password
-
   const [register, setRegister] = useState<boolean>(true);
+
+  // navigator
+  const navigate = useNavigate();
 
   return (
     <div className="auth-container">
+      <div className="auth-back-container" onClick={() => navigate('/')}>
+        <AiOutlineRollback />
+        <button className='navigate-home'>Back</button>
+      </div>
+
       {register && <Register changeStatus={(value: boolean) => setRegister(value)} /> }
       {!register && <Login changeStatus={(value: boolean) => setRegister(value)} /> }
     </div>
