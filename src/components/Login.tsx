cda8e3c3c14e94249c582862bf869ad8d355767e
Waffen <waffen.sultanofc@gmail.com>
2023-04-11 16:25:01 +0800

    Tweaked and added new features

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,33 +1,60 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import '../App.css'
+import { auth } from '../routes/Home'
+import { useNavigate } from 'react-router'
 
 function Login(props: any) {
-    const [loginEmail, setLoginEmail] = useState<string>('');
-    const [loginPasword, setLoginPassword] = useState<string>('');
 
+    const [loginEmail, setLoginEmail] = useState<string>('');
+    const [loginPassword, setLoginPassword] = useState<string>('');
+    const [errorExists, setErrorExists] = useState<boolean>(false);
+
+    // navigator
+    const navigate = useNavigate();
+
+    // handle page display
     const handleClick = () => {
         props.changeStatus(true);
     }
     
+    // handle logging in user
+    const loginUser = () => {
+        signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+            .then((user) => {
+                // after 2 seconds; navigate back to home
+                setTimeout(() => {
+                    navigate('/')
+                }, 2000);
+            })
+            .catch((err) => {
+                console.error(err)
+
+                setErrorExists(true);
+
+                setTimeout(() => {
+                    setErrorExists(false);
+                }, 1000);
+            })
+    }
+
     return (
         <article className="authentication">
-            <h1 className='auth-title'>Afin's MD</h1>
-            <p>Welcome back!
-                <span onClick={handleClick} className='inquiry'> Don't have an account already?</span>
-            </p>
+            <p className="create-account-inquiry">Let's login to your account!</p>
             <div className="part-container">
                 <h5>EMAIL *</h5>
                 <input type="email" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value) } />
             </div>
             <div className="part-container">
                 <h5>PASSWORD *</h5>
-                <input type="password" value={loginPasword} onChange={(e) => setLoginPassword(e.target.value)} />
+                <input type="password" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} />
             </div>
-            <div className="button-container">
-                <button>REGISTER</button>
+            <div className={`button-container ${errorExists && 'shake'}`}>
+                <button onClick={loginUser}>LOGIN</button>
             </div>
+
+            <p className="account-inquiry" onClick={handleClick}>Don't have an account yet?</p>
         </article>  
   )
 }
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -3,24 +3,45 @@ import {
     createUserWithEmailAndPassword, 
      } from 'firebase/auth';
 import { useState } from 'react'
+import { auth } from '../routes/Home';
+import { useNavigate } from 'react-router';
 import '../App.css'
 
 function Register(props: any) {
-  // after registration, redirect user to home page
 
   const [registerEmail, setRegisterEmail] = useState<string>('');
   const [registerPassword, setRegisterPassword] = useState<string>('');
+  const [errorExists, setErrorExists] = useState<boolean>(false);
+
+  // navigator
+  const navigate = useNavigate();
 
   const handleClick = () => {
       props.changeStatus(false)
   }
 
+  const registerUser = () => {
+    createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+      .then((user) => {
+        // navigate user back to home page
+        setTimeout(() => {
+          navigate('/')
+        }, 2000);
+      })
+      .catch((err) => {
+        console.error(err);
+
+        setErrorExists(true);
+
+        setTimeout(() => {
+            setErrorExists(false);
+        }, 1000);
+      })
+  }
+
   return (
     <article className="authentication">
-      <h1 className='auth-title'>Afin's MD</h1>
-      <p>Sign up now.  
-          <span onClick={handleClick} className='inquiry'> Already have an account?</span>
-      </p>
+      <p className='create-account-inquiry'>Let's create your account!</p>
       <div className="part-container">
         <h5>EMAIL *</h5>
         <input type="email" value={registerEmail} onChange={(e) => setRegisterEmail(e.target.value) } />
@@ -29,9 +50,11 @@ function Register(props: any) {
         <h5>PASSWORD *</h5>
         <input type="password" value={registerPassword} onChange={(e) => setRegisterPassword(e.target.value)} />
       </div>
-      <div className="button-container">
-        <button>REGISTER</button>
+      <div className={`button-container ${errorExists && 'shake'}`} >
+        <button onClick={registerUser}>REGISTER</button>
       </div>
+
+      <p onClick={handleClick} className='account-inquiry'>Already have an account?</p>
     </article>
   )
 }
